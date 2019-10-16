c71cc4c97feca5fc6a3a59ef3f9e8c303e5a0407
sayyidmaulana <sayyid.cakap@gmail.com>
2019-10-16 23:13:44 +0700

    Initial Commit

---
+++ DaftarBuku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DaftarBuku
+//
+//  Created by sayyid maulana khakul yakin on 10/16/19.
+//  Copyright © 2019 sayyid maulana khakul yakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
