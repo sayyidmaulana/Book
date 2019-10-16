c2db0310071ca82cf5a941b9bcbec0b045ce2479
sayyidmaulana <sayyid.cakap@gmail.com>
2019-10-17 01:34:18 +0700

    first commit

---
+++ DaftarBuku/MainView/MainView.swift
@@ -0,0 +1,9 @@
+//
+//  MainView.swift
+//  DaftarBuku
+//
+//  Created by sayyid maulana khakul yakin on 10/16/19.
+//  Copyright © 2019 sayyid maulana khakul yakin. All rights reserved.
+//
+
+import Foundation
